1530753dde6437f22aa7e94d943df741b6faf353
Tjaž Eržen <erzen.tjaz@gmail.com>
2019-08-13 16:31:08 +0200

    Explanation of helper functions

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,7 @@
 from math import sqrt
 
 
+# using euclidean distance for getting the step distances and also estimating the path cost to the goal
 def calculate_distance(x1, y1, x2, y2):
     d1 = (x1 - x2)**2
     d2 = (y1 - y2)**2
@@ -8,9 +9,11 @@ def calculate_distance(x1, y1, x2, y2):
     return distance
 
 
-def heuristic_value(point, coordinates):
-    x1 = coordinates[point][0]
-    y1 = coordinates[point][1]
+# calculate the heuristic value from the point which will the the end point of our search algorithm to all the rest point.
+# This will serve as an estimation of the distance
+def heuristic_value(end_point, coordinates):
+    x1 = coordinates[end_point][0]
+    y1 = coordinates[end_point][1]
     lst = []
     for i in range(len(coordinates)):
         x2 = coordinates[i][0]
@@ -20,6 +23,7 @@ def heuristic_value(point, coordinates):
     return lst
 
 
+# This function will calculate the distance from one node to its neighbours (or children) and put them into a list for later use
 def calculate_connections(coordinates, connection_list):
     lst = connection_list
     for i in range(len(connection_list)):
